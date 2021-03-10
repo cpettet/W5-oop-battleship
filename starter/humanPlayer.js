15126d6c276efe1feb9a200c94ac3f5e8570685d
Danny <dchen284@gmail.com>
2021-03-10 14:55:38 -0600

    start Phase 1

--- starter/humanPlayer.js
+++ starter/humanPlayer.js
@@ -1,14 +1,25 @@
+const readline = require("readline");
+
 class HumanPlayer {
   constructor() {
     // TODO: Create a new readline interface and store is as an instance
     // variable (this.rl). Remember to require the built-in node module,
     // 'readline'
+
+    this.rl = readline.createInterface(process.stdin, process.stdout);
   }
 
-  getMove() {
+  getMove(processMove) {
     // TODO: Ask the user for their move and process the answer using the rl
     // interface.Invoke the callback function (processMove), passing in
     // the given answer in the form of an array representing [row, col]
+
+    this.rl.question("What is your move? (in the form of: row, col)", (answer) => {
+      //turn answer into an array
+      let [row, col] = answer.split(", ")
+      processMove([row, col]);
+      this.rl.close();
+    })
   }
 
   processGameOver(isWon) {
